4ac69005da28f60223ca777182cbdcfe0bda8fdc
Your Name <Dominic.Schu@proreplicate.com>
2025-04-22 13:59:18 +0200

    Aktualisiert

--- Konzepte/Wetter_Warnsystem/Simulationen/schu_equation.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-def schu_equation(temperature, resonance_value, constant_e=2.718, constant_pi=3.14159):
-    energy_transfer = constant_pi * constant_e * resonance_value * temperature
-    return energy_transfer
